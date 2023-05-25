f1fb6f838656950370f9225eaba0b126e77fe7f8
Tymek Gryszkalis <lalarsek@gmail.com>
2023-05-25 19:32:24 +0200

    prompt config

--- ps_user_profile.ps1
+++ ps_user_profile.ps1
@@ -1,6 +1,10 @@
 # Prompt
 oh-my-posh init pwsh | Invoke-Expression
 
+# Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'tg.omp.json'
+
 # Alias
 Set-Alias vim nvim
 Set-Alias ll ls
